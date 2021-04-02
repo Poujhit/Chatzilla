a916b3422a4800e80f1391bfa077297b80439e1b
NikhileshJr08 <nikhileshjr08@gmail.com>
2021-04-02 12:20:37 +0530

    Initialized express and add the node module files to the server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log('Listening');
+});
+
+app.get('/', (req,res) => {
+  res.send("Hello")
+});
 console.log('hello world');
