9e2a09536e659a96cc826a031aae96f48a499040
Poujhit <poujhit.dev@gmail.com>
2021-04-02 16:44:17 +0530

    Added socket in server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+const router = require('./router');
+
 const app = express();
+const server = http.createServer(app);
+
+const io = socketio(server);
+
 const PORT = 5000 || process.env.PORT;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.use(router);
+
+io.on('connection', (socket) => {
+	console.log('connection is done');
+
+	socket.on('disconnet', () => {
+		console.log('user left');
+	});
 });
 
-app.get('/', (req,res) => {
-  res.send("Hello")
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
 });
-console.log('hello world');
