7e9cab3dedb77d3791cdceceb345f23d23d34df2
Poujhit <poujhit.dev@gmail.com>
2021-04-05 21:48:02 +0530

    Server is completely finished

--- server/index.js
+++ server/index.js
@@ -1,25 +1,67 @@
+const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-const http = require('http');
+const cors = require('cors');
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
+
 const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
-
 const io = socketio(server);
 
-const PORT = 5000 || process.env.PORT;
-
+app.use(cors());
 app.use(router);
 
-io.on('connection', (socket) => {
-	console.log('connection is done');
+io.on('connect', (socket) => {
+	socket.on('join', ({ name, room }, callback) => {
+		const { error, user } = addUser({ id: socket.id, name, room });
 
-	socket.on('disconnet', () => {
-		console.log('user left');
+		if (error) return callback(error);
+
+		socket.join(user.room);
+
+		socket.emit('message', {
+			user: 'admin',
+			text: `${user.name}, welcome to room ${user.room}.`,
+		});
+		socket.broadcast
+			.to(user.room)
+			.emit('message', { user: 'admin', text: `${user.name} has joined!` });
+
+		io.to(user.room).emit('roomData', {
+			room: user.room,
+			users: getUsersInRoom(user.room),
+		});
+
+		callback();
+	});
+
+	socket.on('sendMessage', (message, callback) => {
+		const user = getUser(socket.id);
+
+		io.to(user.room).emit('message', { user: user.name, text: message });
+
+		callback();
+	});
+
+	socket.on('disconnect', () => {
+		const user = removeUser(socket.id);
+
+		if (user) {
+			io.to(user.room).emit('message', {
+				user: 'Admin',
+				text: `${user.name} has left.`,
+			});
+			io.to(user.room).emit('roomData', {
+				room: user.room,
+				users: getUsersInRoom(user.room),
+			});
+		}
 	});
 });
 
-app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
-});
+server.listen(process.env.PORT || 5000, () =>
+	console.log(`Server has started.`)
+);
