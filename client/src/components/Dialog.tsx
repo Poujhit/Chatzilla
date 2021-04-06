8227fdba38362cb99a248cd4416fff3cf15948b8
Poujhit <poujhit.dev@gmail.com>
2021-04-06 14:39:43 +0530

    Added dialog component

---
+++ client/src/components/Dialog.tsx
@@ -0,0 +1,47 @@
+import {
+	Button,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogContentText,
+	DialogTitle,
+} from '@material-ui/core';
+import React from 'react';
+
+interface PopUpDialogProps {
+	open: boolean;
+	onClose: () => void;
+	title: string;
+	content: string;
+	onOkHandled: React.MouseEventHandler<HTMLButtonElement>;
+	okButtonText: string;
+	notOkButtonText: string;
+}
+
+const PopUpDialog: React.FC<PopUpDialogProps> = (props) => {
+	return (
+		<Dialog
+			open={props.open}
+			onClose={props.onClose}
+			aria-labelledby='alert-dialog-title'
+			aria-describedby='alert-dialog-description'
+		>
+			<DialogTitle id='alert-dialog-title'>{props.title}</DialogTitle>
+			<DialogContent>
+				<DialogContentText id='alert-dialog-description'>
+					{props.content}
+				</DialogContentText>
+			</DialogContent>
+			<DialogActions>
+				<Button onClick={props.onClose} color='primary'>
+					{props.notOkButtonText}
+				</Button>
+				<Button onClick={props.onOkHandled} color='primary' autoFocus>
+					{props.okButtonText}
+				</Button>
+			</DialogActions>
+		</Dialog>
+	);
+};
+
+export default PopUpDialog;
