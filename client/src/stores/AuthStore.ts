7a8606e36c42e5c49b0a29713d95fdbc3a9c0976
Poujhit <poujhit.dev@gmail.com>
2021-04-06 14:39:23 +0530

    Code prettiyfied

--- client/src/stores/AuthStore.ts
+++ client/src/stores/AuthStore.ts
@@ -1,17 +1,17 @@
 import create from 'zustand';
 
-type State={
-    isLogin:boolean;
-    setSignUp:(isSignUp:boolean)=>void;
-}
+type State = {
+	isLogin: boolean;
+	setSignUp: (isSignUp: boolean) => void;
+};
 
-const authStore = create<State>((set)=>({
-    isLogin:true,
-    setSignUp:(isSignUp)=> set((state)=>({
-        ...state,
-        isLogin:isSignUp
-    }))
+const authStore = create<State>((set) => ({
+	isLogin: true,
+	setSignUp: (isSignUp) =>
+		set((state) => ({
+			...state,
+			isLogin: isSignUp,
+		})),
 }));
-
 
 export default authStore;
