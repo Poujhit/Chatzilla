84f3d5ef6d2481845ef2cfb725a9ab3b7203bcf6
Poujhit <poujhit.dev@gmail.com>
2021-04-07 22:55:20 +0530

    Fixed some issues in server

--- server/index.js
+++ server/index.js
@@ -15,35 +15,29 @@ app.use(cors());
 app.use(router);
 
 io.on('connect', (socket) => {
-	socket.on('join', ({ name, room }, callback) => {
-		const { error, user } = addUser({ id: socket.id, name, room });
-
-		if (error) return callback(error);
+	socket.on('join', ({ name, room }) => {
+		const user = addUser({ id: socket.id, name, room });
 
 		socket.join(user.room);
 
 		socket.emit('message', {
-			user: 'admin',
-			text: `${user.name}, welcome to room ${user.room}.`,
+			user: 'Admin',
+			text: `${user.name}, Welcome to the Chat Room ${user.room}.`,
 		});
 		socket.broadcast
 			.to(user.room)
-			.emit('message', { user: 'admin', text: `${user.name} has joined!` });
+			.emit('message', { user: 'Admin', text: `${user.name} has joined!` });
 
 		io.to(user.room).emit('roomData', {
 			room: user.room,
 			users: getUsersInRoom(user.room),
 		});
-
-		callback();
 	});
 
-	socket.on('sendMessage', (message, callback) => {
+	socket.on('sendMessage', (message) => {
 		const user = getUser(socket.id);
 
 		io.to(user.room).emit('message', { user: user.name, text: message });
-
-		callback();
 	});
 
 	socket.on('disconnect', () => {
--- server/users.js
+++ server/users.js
@@ -15,7 +15,7 @@ const addUser = ({ id, name, room }) => {
 
 	users.push(user);
 
-	return { user };
+	return user;
 };
 
 const removeUser = (id) => {
