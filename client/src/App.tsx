0dd24f454438f2151956d1c7fca58b449531f899
Poujhit <poujhit.dev@gmail.com>
2021-06-25 20:41:32 +0530

    Added electron support

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,18 +8,18 @@ import CreateChatRoomScreen from './containers/chat/CreateChatRoom';
 import './App.css';
 
 const App: React.FC = () => {
-	return (
-		<BrowserRouter>
-			<div className='App'>
-				<Switch>
-					<Route path='/create-chat-room:id' component={CreateChatRoomScreen} />
-					<Route path='/chat-room:roomname' component={ChatScreen} />
-					<Route path='/' exact component={AuthScreen} />
-					<Redirect to='/' path='/' />
-				</Switch>
-			</div>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <div className='App'>
+        <Switch>
+          <Route path='/create-chat-room:id' component={CreateChatRoomScreen} />
+          <Route path='/chat-room:roomname' component={ChatScreen} />
+          <Route path='/' exact component={AuthScreen} />
+          <Redirect to='/' path='/' />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- client/src/containers/chat/ChatScreen/ChatScreen.tsx
+++ client/src/containers/chat/ChatScreen/ChatScreen.tsx
@@ -14,142 +14,142 @@ import roomDataStore from '../../../stores/RoomDataStore';
 let socket: Socket<DefaultEventsMap, DefaultEventsMap>;
 
 export type Message = {
-	user: string;
-	text: string;
+  user: string;
+  text: string;
 };
 
 export type User = {
-	room: string;
-	users: [
-		{
-			id: string;
-			name: string;
-			room: string;
-		}
-	];
+  room: string;
+  users: [
+    {
+      id: string;
+      name: string;
+      room: string;
+    }
+  ];
 };
 
 const ChatScreen: React.FC = () => {
-	const classes = useChatScreenStyles();
+  const classes = useChatScreenStyles();
 
-	const roomData = roomDataStore((state) => state);
+  const roomData = roomDataStore((state) => state);
 
-	const isAuthenticated = userDataStore((state) => state.isAuthenticated);
+  const isAuthenticated = userDataStore((state) => state.isAuthenticated);
 
-	const [messages, setMessages] = useState<Message[]>([]);
-	const [users, setUsers] = useState<User>();
-	const [isLoading, setLoading] = useState(true);
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [users, setUsers] = useState<User>();
+  const [isLoading, setLoading] = useState(true);
 
-	const history = useHistory();
+  const history = useHistory();
 
-	useEffect(() => {
-		if (isAuthenticated) {
-			const connectionOptions: any = {
-				'force new connection': true,
-				reconnectionAttempts: 'Infinity',
-				timeout: 10000,
-				transports: ['websocket'],
-			};
+  useEffect(() => {
+    if (isAuthenticated) {
+      const connectionOptions: any = {
+        'force new connection': true,
+        reconnectionAttempts: 'Infinity',
+        timeout: 10000,
+        transports: ['websocket'],
+      };
 
-			socket = io(
-				'https://private-chat-app-server.herokuapp.com/',
-				connectionOptions
-			);
+      socket = io(
+        'https://private-chat-app-server.herokuapp.com/',
+        connectionOptions
+      );
 
-			socket.emit('join', { name: roomData.name, room: roomData.room });
-		}
-	}, [isAuthenticated, roomData]);
+      socket.emit('join', { name: roomData.name, room: roomData.room });
+    }
+  }, [isAuthenticated, roomData]);
 
-	useEffect(() => {
-		if (isAuthenticated) {
-			socket.on('message', (message: Message) => {
-				setMessages((messages) => [...messages, message]);
-			});
+  useEffect(() => {
+    if (isAuthenticated) {
+      socket.on('message', (message: Message) => {
+        setMessages((messages) => [...messages, message]);
+      });
 
-			socket.on('roomData', (usersInroom: User) => {
-				setUsers(usersInroom);
-				setLoading(false);
-			});
-		}
-	}, [isAuthenticated]);
+      socket.on('roomData', (usersInroom: User) => {
+        setUsers(usersInroom);
+        setLoading(false);
+      });
+    }
+  }, [isAuthenticated]);
 
-	const sendMessage = (userTypedMessage: string) => {
-		socket.emit('sendMessage', userTypedMessage);
-	};
-	console.log(isLoading);
+  const sendMessage = (userTypedMessage: string) => {
+    socket.emit('sendMessage', userTypedMessage);
+  };
+  console.log(isLoading);
 
-	return (
-		<div className={classes.Background}>
-			{!isAuthenticated ? (
-				<Card
-					className={classes.Card}
-					style={{
-						justifyContent: 'center',
-						width: '30%',
-						height: '30%',
-						flexDirection: 'column',
-						alignItems: 'center',
-					}}
-				>
-					<Typography
-						style={{
-							fontFamily: 'Rubik',
-							fontWeight: 'bold',
-							fontSize: '20px',
-							color: 'darkblue',
-						}}
-					>
-						You are not authenticated. Go to the home page to login.
-					</Typography>
-					<Button
-						className={classes.sendButton}
-						onClick={() => history.replace('/')}
-					>
-						Ok
-					</Button>
-				</Card>
-			) : (
-				<Card className={classes.Card}>
-					<div className={classes.leftPortionOfCard}>
-						<Typography className={classes.title}>
-							Users in this chat room:
-						</Typography>
-						<UserList users={users} />
-					</div>
-					<div className={classes.chatPortion}>
-						<div className={classes.statusBar}>
-							<Typography className={classes.statusBarTitle}>
-								{`${roomData.room} chat room`}
-							</Typography>
-							<br />
-							<Button
-								className={classes.closeButton}
-								onClick={() => {
-									history.replace('/');
-								}}
-							>
-								X
-							</Button>
-						</div>
-						{isLoading ? (
-							<div
-								style={{
-									marginTop: '40%',
-								}}
-							>
-								<CircularProgress />
-							</div>
-						) : (
-							<React.Fragment>
-								<MessagePortion messages={messages} name={roomData.name} />
-								<MessageInputPortion submitChat={sendMessage} />
-							</React.Fragment>
-						)}
-					</div>
-				</Card>
-			)}
-		</div>
-	);
+  return (
+    <div className={classes.Background}>
+      {!isAuthenticated ? (
+        <Card
+          className={classes.Card}
+          style={{
+            justifyContent: 'center',
+            width: '30%',
+            height: '30%',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography
+            style={{
+              fontFamily: 'Rubik',
+              fontWeight: 'bold',
+              fontSize: '20px',
+              color: 'darkblue',
+            }}
+          >
+            You are not authenticated. Go to the home page to login.
+          </Typography>
+          <Button
+            className={classes.sendButton}
+            onClick={() => history.replace('/')}
+          >
+            Ok
+          </Button>
+        </Card>
+      ) : (
+        <Card className={classes.Card}>
+          <div className={classes.leftPortionOfCard}>
+            <Typography className={classes.title}>
+              Users in this chat room:
+            </Typography>
+            <UserList users={users} />
+          </div>
+          <div className={classes.chatPortion}>
+            <div className={classes.statusBar}>
+              <Typography className={classes.statusBarTitle}>
+                {`${roomData.room} chat room`}
+              </Typography>
+              <br />
+              <Button
+                className={classes.closeButton}
+                onClick={() => {
+                  history.replace('/');
+                }}
+              >
+                X
+              </Button>
+            </div>
+            {isLoading ? (
+              <div
+                style={{
+                  marginTop: '40%',
+                }}
+              >
+                <CircularProgress />
+              </div>
+            ) : (
+              <React.Fragment>
+                <MessagePortion messages={messages} name={roomData.name} />
+                <MessageInputPortion submitChat={sendMessage} />
+              </React.Fragment>
+            )}
+          </div>
+        </Card>
+      )}
+    </div>
+  );
 };
 
 export default ChatScreen;
