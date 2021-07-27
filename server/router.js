9640f31a56d3c45c2f8a31eef780cd76118aba73
Poujhit μ <poujhit.dev@gmail.com>
2021-07-27 20:10:29 +0530

    Update router.js

--- server/router.js
+++ server/router.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	res.send('server is running.');
+	res.send('server is running!');
 });
 
 module.exports = router;
