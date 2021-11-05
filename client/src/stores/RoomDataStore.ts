1e367fede75a1a1033fc8e4a390f658834e56471
Poujhit <poujhit.dev@gmail.com>
2021-11-05 23:51:18 +0530

    Removed unused stores and added persist to room data

--- client/src/stores/RoomDataStore.ts
+++ client/src/stores/RoomDataStore.ts
@@ -1,4 +1,5 @@
 import create from 'zustand';
+import { persist } from 'zustand/middleware';
 
 type State = {
   name: string;
@@ -7,25 +8,32 @@ type State = {
   setRoom: (room: string) => void;
 };
 
-const roomDataStore = create<State>((set) => ({
-  name: '',
-  room: '',
-  setName: (newname) =>
-    set(
-      (state) => ({
-        ...state,
-        name: newname,
-      }),
-      true
-    ),
-  setRoom: (roomname) =>
-    set(
-      (state) => ({
-        ...state,
-        room: roomname,
-      }),
-      true
-    ),
-}));
+const roomDataStore = create<State>(
+  persist(
+    (set) => ({
+      name: '',
+      room: '',
+      setName: (newname) =>
+        set(
+          (state) => ({
+            ...state,
+            name: newname,
+          }),
+          true
+        ),
+      setRoom: (roomname) =>
+        set(
+          (state) => ({
+            ...state,
+            room: roomname,
+          }),
+          true
+        ),
+    }),
+    {
+      name: 'chatzilla-room-data',
+    }
+  )
+);
 
 export default roomDataStore;
