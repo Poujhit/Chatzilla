35e35cb93fdb57b6ea851bc86a2b3a43309091b6
Poujhit <poujhit.dev@gmail.com>
2021-11-05 23:51:30 +0530

    Added a new bottom bar component

---
+++ client/src/components/BottomBar/BottomBar.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+interface BottomBarProps {}
+
+const BottomBar: React.FC<BottomBarProps> = (props) => {
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        color: 'white',
+        left: '50%',
+        cursor: 'pointer',
+        bottom: '20px',
+        transform: 'translate(-50%, -50%)',
+        margin: '0 auto',
+      }}
+      onClick={() => {
+        window.open(
+          'https://www.github.com/Poujhit',
+          '_blank',
+          'noopener,noreferrer'
+        );
+      }}
+    >
+      Made by Poujhit
+    </div>
+  );
+};
+export default BottomBar;
