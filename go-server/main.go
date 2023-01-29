f958b23615e5c1d59d781787221eae1fa184245f
Poujhit <poujhit.dev@gmail.com>
2023-01-29 20:18:43 +0530

    small fix

--- go-server/main.go
+++ go-server/main.go
@@ -4,15 +4,28 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 
 	socketio "github.com/googollee/go-socket.io"
 	"github.com/googollee/go-socket.io/engineio"
+	"github.com/googollee/go-socket.io/engineio/transport"
+	"github.com/googollee/go-socket.io/engineio/transport/polling"
+	"github.com/googollee/go-socket.io/engineio/transport/websocket"
 )
+
+var allowOriginFunc = func(r *http.Request) bool {
+	return true
+}
 
 func main() {
 	server := socketio.NewServer(&engineio.Options{
-		PingTimeout: time.Duration(30000),
+		Transports: []transport.Transport{
+			&polling.Transport{
+				CheckOrigin: allowOriginFunc,
+			},
+			&websocket.Transport{
+				CheckOrigin: allowOriginFunc,
+			},
+		},
 	})
 
 	server.OnConnect("/", func(s socketio.Conn) error {
@@ -22,37 +35,46 @@ func main() {
 		return nil
 	})
 
+	server.OnEvent("/", "join", func(s socketio.Conn, msg map[string]string) {
+		s.SetContext(msg)
+
+		fmt.Println("msg:", msg)
+
+		// s.Emit("reply", "have "+msg)
+	})
+
 	// server.BroadcastToRoom()
 
-	server.OnEvent("/", "notice", func(s socketio.Conn, msg string) {
-		fmt.Println("notice:", msg)
-		s.Emit("reply", "have "+msg)
-	})
+	// server.OnEvent("/", "notice", func(s socketio.Conn, msg string) {
+	// 	fmt.Println("notice:", msg)
 
-	server.OnEvent("/chat", "msg", func(s socketio.Conn, msg string) string {
-		s.SetContext(msg)
-		return "recv " + msg
-	})
+	// 	s.Emit("reply", "have "+msg)
+	// })
 
-	server.OnEvent("/", "bye", func(s socketio.Conn) string {
-		last := s.Context().(string)
-		s.Emit("bye", last)
-		s.Close()
-		return last
-	})
+	// server.OnEvent("/chat", "msg", func(s socketio.Conn, msg string) string {
+	// 	s.SetContext(msg)
+	// 	return "recv " + msg
+	// })
 
-	server.OnError("/", func(s socketio.Conn, e error) {
-		fmt.Println("meet error:", e)
-	})
+	// server.OnEvent("/", "bye", func(s socketio.Conn) string {
+	// 	last := s.Context().(string)
+	// 	s.Emit("bye", last)
+	// 	s.Close()
+	// 	return last
+	// })
 
-	server.OnDisconnect("/", func(s socketio.Conn, reason string) {
-		fmt.Println("closed", reason)
-	})
+	// server.OnError("/", func(s socketio.Conn, e error) {
+	// 	fmt.Println("meet error:", e)
+	// })
+
+	// server.OnDisconnect("/", func(s socketio.Conn, reason string) {
+	// 	fmt.Println("closed", reason)
+	// })
 
 	go server.Serve()
 	defer server.Close()
 
-	http.Handle("/ws", server)
-	log.Println("Serving at localhost:4000...")
-	log.Fatal(http.ListenAndServe(":4000", nil))
+	http.Handle("/socket.io/", server)
+	log.Println("Serving at localhost:8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
