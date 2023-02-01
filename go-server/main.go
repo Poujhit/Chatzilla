f25b18fc323cbe6ca8f155caded7d8bec46b3dbb
Poujhit <poujhit.dev@gmail.com>
2023-02-01 21:49:37 +0530

    added the go code for the socket server

--- go-server/main.go
+++ go-server/main.go
@@ -1,84 +1,110 @@
 package main
 
 import (
+	"chatzilla-server/helpers"
 	"fmt"
-	"log"
-	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
-	socketio "github.com/googollee/go-socket.io"
-	"github.com/googollee/go-socket.io/engineio"
-	"github.com/googollee/go-socket.io/engineio/transport"
-	"github.com/googollee/go-socket.io/engineio/transport/polling"
-	"github.com/googollee/go-socket.io/engineio/transport/websocket"
+	"github.com/zishang520/engine.io/types"
+	"github.com/zishang520/socket.io/socket"
 )
 
-var allowOriginFunc = func(r *http.Request) bool {
-	return true
-}
+// .emit will send to the connected client.
+// in order to send and receive msg from client to server and .emit is used.
+// .broadcast.to msg is sent to all clients in a room except the one client that sends this broadcasted msg.
 
 func main() {
-	server := socketio.NewServer(&engineio.Options{
-		Transports: []transport.Transport{
-			&polling.Transport{
-				CheckOrigin: allowOriginFunc,
-			},
-			&websocket.Transport{
-				CheckOrigin: allowOriginFunc,
-			},
-		},
+	ops := socket.DefaultServerOptions()
+	ops.SetAllowEIO3(true)
+	ops.SetCors(&types.Cors{
+		Origin: "http://localhost:3000",
+		// Credentials: true,
+	})
+	httpServer := types.CreateServer(nil)
+	io := socket.NewServer(httpServer, ops)
+	io.On("connection", func(clients ...any) {
+		fmt.Println(len(clients))
+
+		for i := 0; i < len(clients); i++ {
+			sock := clients[i].(*socket.Socket)
+			fmt.Println(sock.Id())
+		}
+		client := clients[0].(*socket.Socket)
+
+		fmt.Println(client.Handshake().Query.Get("room"))
+		client.On("join", func(datas ...any) {
+			var data map[string]interface{} = datas[0].(map[string]interface{})
+			fmt.Println(data)
+
+			helpers.AddUser(map[string]string{
+				"id":   string(client.Id()),
+				"name": data["name"].(string),
+				"room": data["room"].(string),
+			})
+
+			// why like this because, custom type string is not the same as string, so type casting is not possible
+			// earlier it was like this client.Join(data["room"].(socket.Room))
+			// the old one causes error because interface{} is string, not socket.Room
+			var room string = data["room"].(string)
+			client.Join(socket.Room(room))
+
+			// hover over the function to know why this is used.
+			client.Emit("message", map[string]string{
+				"user": "Admin",
+				"text": fmt.Sprintf("%s, Welcome to the chat room %s", data["name"], data["room"]),
+			})
+
+			// will broadcast the message to everyone except the one who told to send the message.
+			client.Broadcast().To(socket.Room(room)).Emit("message", map[string]string{
+				"user": "Admin",
+				"text": fmt.Sprintf("%s has joined!", data["name"]),
+			})
+
+			roomData := map[string]interface{}{
+				"room":  data["room"],
+				"users": helpers.GetUsersInRoom(data["room"].(string)),
+			}
+			client.Emit("roomData", roomData)
+			client.Broadcast().To(socket.Room(room)).Emit("roomData", roomData)
+		})
+
+		client.On("sendMessage", func(data ...any) {
+			msg := data[0].(string)
+			user := helpers.GetUser(string(client.Id()))
+
+			sendingMsg := map[string]interface{}{
+				"user": user["name"],
+				"text": msg,
+			}
+
+			client.Emit("message", sendingMsg)
+			client.Broadcast().To(socket.Room(user["room"])).Emit("message", sendingMsg)
+		})
+		client.On("disconnect", func(...any) {
+		})
 	})
 
-	server.OnConnect("/", func(s socketio.Conn) error {
-		// s.SetContext("")
-
-		fmt.Println("connected:", s.ID())
-
-		// fmt.Println(s.Rooms())
-
-		// s.
-		return nil
+	httpServer.Listen("127.0.0.1:8080", func() {
+		fmt.Println("listening now")
 	})
 
-	server.OnEvent("/", "join", func(s socketio.Conn, msg map[string]string) {
-		// s.SetContext(msg)
-		fmt.Println("here")
-		fmt.Println("msg:", msg)
+	exit := make(chan struct{})
+	SignalC := make(chan os.Signal)
 
-		// s.Emit("reply", "have "+msg)
-	})
+	signal.Notify(SignalC, os.Interrupt, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	go func() {
+		for s := range SignalC {
+			switch s {
+			case os.Interrupt, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT:
+				close(exit)
+				return
+			}
+		}
+	}()
 
-	// server.BroadcastToRoom()
-
-	// server.OnEvent("/", "notice", func(s socketio.Conn, msg string) {
-	// 	fmt.Println("notice:", msg)
-
-	// 	s.Emit("reply", "have "+msg)
-	// })
-
-	// server.OnEvent("/chat", "msg", func(s socketio.Conn, msg string) string {
-	// 	s.SetContext(msg)
-	// 	return "recv " + msg
-	// })
-
-	// server.OnEvent("/", "bye", func(s socketio.Conn) string {
-	// 	last := s.Context().(string)
-	// 	s.Emit("bye", last)
-	// 	s.Close()
-	// 	return last
-	// })
-
-	// server.OnError("/", func(s socketio.Conn, e error) {
-	// 	fmt.Println("meet error:", e)
-	// })
-
-	// server.OnDisconnect("/", func(s socketio.Conn, reason string) {
-	// 	fmt.Println("closed", reason)
-	// })
-
-	go server.Serve()
-	defer server.Close()
-
-	http.Handle("/socket.io/", server)
-	log.Println("Serving at localhost:8080...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	<-exit
+	httpServer.Close(nil)
+	os.Exit(0)
 }
