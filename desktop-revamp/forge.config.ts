d736f17bf4867265755aab0a68cd3478fcc03755
Poujhit <poujhit.dev@gmail.com>
2023-02-11 13:33:12 +0530

    Added icons

--- desktop-revamp/forge.config.ts
+++ desktop-revamp/forge.config.ts
@@ -9,9 +9,16 @@ import { mainConfig } from './webpack.main.config';
 import { rendererConfig } from './webpack.renderer.config';
 
 const config: ForgeConfig = {
-  packagerConfig: {},
+  packagerConfig: {
+    icon: './src/images/icon',
+  },
   rebuildConfig: {},
-  makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
+  makers: [
+    new MakerSquirrel({}),
+    new MakerZIP({}, ['darwin']),
+    new MakerRpm({}),
+    new MakerDeb({}),
+  ],
   plugins: [
     new WebpackPlugin({
       mainConfig,
